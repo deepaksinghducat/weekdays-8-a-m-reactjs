d3ca4d927bb3aac8cea4aaec99d6589d458a0a69
unknown <deepaksinghgusain>
2022-10-05 08:56:49 +0530

    05-10-2022

---
+++ js/basic.js
@@ -0,0 +1,48 @@
+// console.log("Fasdfasdf");
+
+// display 
+
+// console
+// console.log("fasdfasf");
+
+// document
+// document.write("fasdfasf")
+
+// alert 
+
+// alert('fsdafadfs');
+
+// prompt
+
+// var getName = prompt("enter your name");
+
+// console.log(getName);
+
+// confirm
+
+// let bool = confirm("are you sure you want to delete shomething");
+
+// console.log(bool);
+
+
+// variables 
+
+// var 
+
+// var dasfddasf = "Fasdfdasf";
+
+// fasasfasdfsadf = "fdsafasfdasfasdf";
+
+// console.log(fasasfasdfsadf);
+
+// let 
+
+// let a = 10;
+// a = "Fasdfdasf";
+
+// console.log(a);
+
+// const
+// const fullname = "fsadfasdsafasfasdfsafadsf";
+// fullname = "Fasdfdasfdsaf";
+
